bcb30a376311432e504fc5cbbfd6dc9e8f648bd6
Gonzalo Veron <gonzalo.pygove@gmail.com>
2024-03-06 10:07:40 -0300

    "Actualización"

--- ta-te-ti/src/App.jsx
+++ ta-te-ti/src/App.jsx
@@ -4,17 +4,25 @@ import { Square } from "./components/Square.jsx"
 import { TURNS} from "./constants.js"
 import { checkWinnerFrom } from "./logic/board.js"
 import { WinnerModal, checkEndGame } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)})
+  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X})
   //null no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
-  
+
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -27,6 +35,13 @@ function App() {
     //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //guardamos partida
+    saveGameToStorage({
+      board:newBoard, 
+      turn:newTurn
+    })
+
     //revisamos si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
